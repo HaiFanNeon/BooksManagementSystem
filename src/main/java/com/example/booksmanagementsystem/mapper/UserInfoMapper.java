d6f177f8c8225deede898da9ab20d5fba1e02394
HaiFanNeon <haifan0312@163.com>
2024-11-27 20:52:50 +0800

    mybatis

--- src/main/java/com/example/booksmanagementsystem/mapper/UserInfoMapper.java
+++ src/main/java/com/example/booksmanagementsystem/mapper/UserInfoMapper.java
@@ -2,23 +2,23 @@ package com.example.booksmanagementsystem.mapper;
 
 
 import com.example.booksmanagementsystem.model.UserInfo;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface UserInfoMapper {
 
-    @Select("select * from userinfo")
     List<UserInfo> selectAll();
 
     @Select("select * from userinfo where id = #{userid}")
     UserInfo selectById(@Param("userid") Integer id);
 
-    @Insert("insert into userinfo (username, password, age, gender, phone) values" +
-            " (#{username},#{password},#{age},#{gender},#{phone})")
-    void insert(UserInfo userInfo);
+
+
+    Integer insert(UserInfo userInfo);
+
+    void delete(Integer id);
+
+    void update(UserInfo userInfo);
 }
--- src/test/java/com/example/booksmanagementsystem/mapper/UserInfoMapperTest.java
+++ src/test/java/com/example/booksmanagementsystem/mapper/UserInfoMapperTest.java
@@ -14,16 +14,36 @@ class UserInfoMapperTest {
     private UserInfoMapper userInfoMapper;
 
     @Test
+    public void testSelect() {
+        userInfoMapper.selectAll();
+    }
+
+    @Test
     public void selectById() {
         UserInfo userInfo = userInfoMapper.selectById(2);
         System.out.println(userInfo.toString());
     }
 
     @Test
-    public testInsert() {
-        UserInfo userInfo = UserInfo.builder()
-                .username("test")
-                .password("123456")
-                .build();
+    public void testInsert() {
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername("example_username");
+        userInfo.setPassword("example_password");
+        userInfo.setAge(25);
+        userInfo.setGender(1);
+        userInfo.setPhone("12345678901");
+        Integer insert = userInfoMapper.insert(userInfo);
+        System.out.println("新增数据：" + insert + "，数据id：" + userInfo.getId());
+    }
+    @Test
+    public void testDelete() {
+        userInfoMapper.delete(2);
+    }
+
+    @Test
+    public void testUpdate() {
+        UserInfo userInfo = userInfoMapper.selectById(2);
+        userInfo.setAge(30);
+        userInfoMapper.update(userInfo);
     }
 }
